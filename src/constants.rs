89332bff89740ff5f3715a0848ee94f135616c85
Conor Johnston <chjohnston96@gmail.com>
2023-05-23 20:53:39 -0400

    rm lint warning

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
-pub const ALPACA_API_URL: &str = "https://paper-api.alpaca.markets";
+// pub const ALPACA_TRADE_URL: &str = "https://paper-api.alpaca.markets";
 pub const ALPACA_DATA_URL: &str = "https://data.alpaca.markets/v2";
